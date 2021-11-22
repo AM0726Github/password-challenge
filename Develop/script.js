eccaa1b973dc371e3b29f0f167bfc7edb173a19b
AM0726Github <Margaryanalik7@gmail.com>
2021-11-21 19:14:20 -0800

    adding logiks for random password generation

--- Develop/script.js
+++ Develop/script.js
@@ -1,15 +1,15 @@
-// Assignment code here
 // adding constant string containers for all symbols  
 const specialCharacters = " !#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 const numbersString = "0123456789";
 const upperString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const lowerString = "abcdefghijklmnopqrstuvwxyz";
 
-
+// adding content for password generator function
 var generatePassword = function(){
   // each time when user will click button we need to reset passward
   var password = "";
   var passwordDemo = "";
+  var criteriaCount = 0; 
 
   // ask for password length
   var passwordLength = window.prompt("Choose the length of password! enter length betwin 8 and 128");
@@ -35,7 +35,7 @@ var generatePassword = function(){
   var promptLower = window.confirm("Would you like to add any lowerercase latters to your password such as " + lowerString + " latters Enter 'OK' or 'Cancel'");
   
   // ask for at least one criteria
-  while(promptChar == false && promptNumb == false && promptUpper == false && promptLower == false){
+  while(promptChar === false && promptNumb === false && promptUpper === false && promptLower === false){
     window.alert("You need to accept at least one caracter");
     var promptChar = window.confirm("Would you like to add any special characters to your password such as " + specialCharacters + " symbols Enter 'OK' or 'Cancel'");
     var promptNumb = window.confirm("Would you like to add any number to your password such as " + numbersString +" numbers Enter 'OK' or 'Cancel'");
@@ -43,17 +43,42 @@ var generatePassword = function(){
     var promptLower = window.confirm("Would you like to add any lowerercase latters to your password such as " + lowerString + " latters Enter 'OK' or 'Cancel'");
   };
 
-  // 
+  // counting criterias
+  if(promptChar){
+    criteriaCount = criteriaCount + 1;
+    passwordDemo = passwordDemo + specialCharacters;
+  };
+  if(promptNumb){
+    criteriaCount = criteriaCount + 1;
+    passwordDemo = passwordDemo + numbersString;
+  };
+  if(promptUpper){
+    criteriaCount = criteriaCount + 1;
+    passwordDemo = passwordDemo + upperString;
+  };
+  if(promptLower){
+    criteriaCount = criteriaCount + 1;
+    passwordDemo = passwordDemo + lowerString;
+  };
+
+
+  // for(var i = 0 ; i < parseInt(passwordLength); i++){
+  //   passwordDemo[i] =  numbersString[Math.floor(Math.random()*numbersString.length + 1)];
+  // }
+
+  // for(var i = 0 ; i < parseInt(passwordLength); i++){
+  //   passwordDemo[i] =  specialCharacters[Math.floor(Math.random()*specialCharacters.length + 1)];
+  // }
 
   // generate random password by selected criteria
   for(var i = 0 ; i < parseInt(passwordLength); i++){
-
+    var rnum = Math.floor(Math.random() * passwordDemo.length);
+    password = password + passwordDemo.substring(rnum,rnum+1);
   }
 
   // returning generated password 
   return password;
 }
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
